ea51ceb8f4190dc7891fab845152f886021f0ed0
Julia <julia-lelik@mail.ru>
2022-10-29 22:04:33 +0300

    добавили файл

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string[] array1 = new string[5] {"456", "45", "hello", "world", "-2"};
+string[] array2 = new string[array1.Length];
+
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+         Console.Write($"{array2[count]} ");
+        count++;
+        }   
+        Console.WriteLine(); 
+    
+
+
+
+
+
+
+
+    
